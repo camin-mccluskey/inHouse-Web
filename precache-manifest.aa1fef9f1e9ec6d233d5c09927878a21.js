5129933764a3104b47351559801f99fe52a16306
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2020-07-20 00:27:44 +0100

    Updates

---
+++ precache-manifest.aa1fef9f1e9ec6d233d5c09927878a21.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bdf3a28a8d144e8b59093d6bdac0f1",
+    "url": "/inHouse-Web/index.html"
+  },
+  {
+    "revision": "5503a82febdbf935ad8a",
+    "url": "/inHouse-Web/static/css/main.443f0ab0.chunk.css"
+  },
+  {
+    "revision": "baf3590726e874cd153b",
+    "url": "/inHouse-Web/static/js/2.895b51d9.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/inHouse-Web/static/js/2.895b51d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5503a82febdbf935ad8a",
+    "url": "/inHouse-Web/static/js/main.daf326a1.chunk.js"
+  },
+  {
+    "revision": "e032dec882ce6d7cd79b",
+    "url": "/inHouse-Web/static/js/runtime-main.f057e548.js"
+  },
+  {
+    "revision": "81bb07a3f5018bdfd3cca5be7e6632e4",
+    "url": "/inHouse-Web/static/media/app-store-download.81bb07a3.svg"
+  },
+  {
+    "revision": "27284773f69f090afdf05f1814875dda",
+    "url": "/inHouse-Web/static/media/chat-icon.27284773.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/inHouse-Web/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "09e479efcd0087f832ac51849993dd49",
+    "url": "/inHouse-Web/static/media/expenses-demo-resize.09e479ef.png"
+  },
+  {
+    "revision": "9c1da5f64a30ff52f7ee59ad9b6d5e56",
+    "url": "/inHouse-Web/static/media/expenses-icon.9c1da5f6.svg"
+  },
+  {
+    "revision": "602b375f780de38bd2017a6ca8f9dd48",
+    "url": "/inHouse-Web/static/media/google-play-download.602b375f.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/inHouse-Web/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/inHouse-Web/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "0d3828959eb0918935d1c7fd7fcd4f53",
+    "url": "/inHouse-Web/static/media/inHouse_logo.0d382895.png"
+  },
+  {
+    "revision": "c37e8cb1e27062f7ff6c256e504a93d0",
+    "url": "/inHouse-Web/static/media/notifications-demo.c37e8cb1.png"
+  },
+  {
+    "revision": "2ae87ad0ef65d03e2fcef317fa2ca947",
+    "url": "/inHouse-Web/static/media/tasks-demo.2ae87ad0.png"
+  },
+  {
+    "revision": "d5189e19a017e49696a71141a7171406",
+    "url": "/inHouse-Web/static/media/tasks-icon.d5189e19.svg"
+  },
+  {
+    "revision": "a127d686e127caee59a183a05482e6ab",
+    "url": "/inHouse-Web/static/media/video-placeholder.a127d686.jpg"
+  }
+]);
